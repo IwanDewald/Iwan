56046008fc3c5098b43dea071707bcf01f85fd6e
TimurBekher <bekher.timur@gmail.com>
2018-02-15 21:10:02 +0700

    trying launch webhook

--- constants.py
+++ constants.py
@@ -21,7 +21,7 @@ redact_favorite = u'Выберите свой любимый кофе'
 back = u'Назад'
 back_to_menu = u'Назад'
 
-redact_profile = '\N{BUSTS IN SILHOUETTE}' + 'Редактировать профиль'
+redact_profile = '\N{BUSTS IN SILHOUETTE}' + u'Редактировать профиль'
 order = '\N{TEACUP WITHOUT HANDLE}' + 'Сделать предзаказ'
 
 select_language = u'Выберите язык\nSelect your language '
