2e1840652dc7d42cd1657a4c80cc59cca63e3e9e
IwanDewald <36601213+IwanDewald@users.noreply.github.com>
2018-02-25 12:02:05 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,34 +11,7 @@ import config
 import constants 
 import utils
 import storage
-# import cherrypy
 
-
-# WEBHOOK_HOST = '93.170.131.202'
-# WEBHOOK_PORT = 443 
-# WEBHOOK_LISTEN = '0.0.0.0'  
-
-# WEBHOOK_SSL_CERT = './webhook_cert.pem' 
-# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  
-
-# WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-# WEBHOOK_URL_PATH = "/%s/" % (config.token)
-
-
-# class WebhookServer(object):
-#     @cherrypy.expose
-#     def index(self):
-#         if 'content-length' in cherrypy.request.headers and \
-#                         'content-type' in cherrypy.request.headers and \
-#                         cherrypy.request.headers['content-type'] == 'application/json':
-#             length = int(cherrypy.request.headers['content-length'])
-#             json_string = cherrypy.request.body.read(length).decode("utf-8")
-#             update = telebot.types.Update.de_json(json_string)
-            
-#             bot.process_new_updates([update])
-#             return ''
-#         else:
-#             raise cherrypy.HTTPError(403)
 
 bot = telebot.TeleBot(config.token)
 
@@ -57,7 +30,7 @@ def getMessage():
 def webhook():
     print('webhook')
     bot.remove_webhook()
-    bot.set_webhook(url=URL_PATH, sertificate=open('public.pem', 'r'))
+    bot.set_webhook(url=URL_PATH, certificate=open('public.pem', 'r'))
     return ''
 
 
@@ -215,21 +188,6 @@ def photo(message):
 	print(message.photo[2])
 	bot.send_message(chat_id = message.chat.id, text = message.photo[2])
 
-# bot.remove_webhook()
-
- 
-# bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
-
-# cherrypy.config.update({
-#     'server.socket_host': WEBHOOK_LISTEN,
-#     'server.socket_port': WEBHOOK_PORT,
-#     'server.ssl_module': 'builtin',
-#     'server.ssl_certificate': WEBHOOK_SSL_CERT,
-#     'server.ssl_private_key': WEBHOOK_SSL_PRIV
-# })
-
-# cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
 
 if __name__ == '__main__':
 	app.run(port=os.environ.get('PORT', 5000))
