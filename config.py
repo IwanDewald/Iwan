bc1e37e803b457cca63a1b17175f5e637c80025f
IwanDewald <36601213+IwanDewald@users.noreply.github.com>
2018-03-02 14:29:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
-token='531911280:AAHoFolyb09Fi8PCHNZkOaZmadxwhth4F1U'
+
+token = '531911280:AAHoFolyb09Fi8PCHNZkOaZmadxwhth4F1U'
 shelve_name = 'shelve.db'
 database_name = 'CoffeeBot'
+
+app_address = 'https://50fb2066.ngrok.io/{}'.format(token)
